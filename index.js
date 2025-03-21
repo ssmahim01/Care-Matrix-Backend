732cfb588b1921f54f9282766e1a2b625fd0134a
ssmahim01 <mahimzayn666@gmail.com>
2025-03-21 11:44:05 +0600

    Added the origin of deployed website

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://care-matrix.web.app"
+  ],
   credentials: true,
   optionalSuccessStatus: 200,
 };
