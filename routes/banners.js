70390e87014ede83ec395538749dbbcbb00b620f
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-21 19:48:55 +0600

    Updated - banner get api with query

--- routes/banners.js
+++ routes/banners.js
@@ -6,52 +6,55 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 (async () => {
-    await connectDB()
-})()
+  await connectDB();
+})();
 
 router.post("/", async (req, res) => {
-    try {
-        const banner = req.body
+  try {
+    const banner = req.body;
 
-        if (!collections.banners) {
-            return res.status(500).json({ error: "Database not connected yet!" })
-        }
-
-        const result = await collections.banners.insertOne(banner)
-        res.send(result)
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
-router.get('/', async (req, res) => {
     if (!collections.banners) {
-        return res.status(500).json({ error: "Banners collection is not available." });
+      return res.status(500).json({ error: "Database not connected yet!" });
     }
 
-    try {
-        const result = await collections.banners.find().toArray();
-        res.send(result);
-    } catch (error) {
-        console.error("Error fetching banners:", error);
-        res.status(500).json({ error: error.message });
-    }
+    const result = await collections.banners.insertOne(banner);
+    res.send(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/", async (req, res) => {
+  const isActive = req.query.isActive || "active";
+  if (!collections.banners) {
+    return res
+      .status(500)
+      .json({ error: "Banners collection is not available." });
+  }
+
+  try {
+    const query = isActive === "active" ? { status: "active" } : {};
+    const result = await collections.banners.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching banners:", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.patch("/status/:id", async (req, res) => {
-    const id = req.params.id
-    const { status } = req.body
-    // console.log(status, id);
-    const filter = { _id: new ObjectId(id) }
-    const updatedDoc = {
-        $set: {
-            status: status
-        }
-    }
-    // console.log(status);
-    const result = await collections.banners.updateOne(filter, updatedDoc)
-    res.send(result)
+  const id = req.params.id;
+  const { status } = req.body;
+  // console.log(status, id);
+  const filter = { _id: new ObjectId(id) };
+  const updatedDoc = {
+    $set: {
+      status: status,
+    },
+  };
+  // console.log(status);
+  const result = await collections.banners.updateOne(filter, updatedDoc);
+  res.send(result);
 });
-
-
 
 export default router;
