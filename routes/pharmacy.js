f053f74834c3f119ce26af12c81adcc5f99eb7d0
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-22 15:40:26 +0600

    medicines get api route added

--- routes/pharmacy.js
+++ routes/pharmacy.js
@@ -17,8 +17,31 @@ async function initCollection() {
 }
 await initCollection();
 
-router.get("/", async (req, res) => {
-  res.send("All pharmacy data");
+router.get("/medicines", async (req, res) => {
+  try {
+    const search = req.query.search || "";
+    const category = req.query.category || "All Medicines";
+
+    const query = {};
+    if (category !== "All Medicines") query.category = category;
+    if (search) query.brandName = { $regex: search, $options: "i" };
+
+    const result = await medicinesCollection.find(query).toArray();
+    const medicines = result.map((medicine) => ({
+      _id: medicine?._id,
+      brandName: medicine?.brandName,
+      strength: medicine?.strength,
+      price: {
+        amount: medicine?.price?.amount,
+        discountedAmount: medicine?.price?.discount?.discountedAmount,
+      },
+      imageURL: medicine?.imageURL,
+    }));
+    
+    res.send(medicines);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 export default router;
