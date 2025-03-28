1dd73e4365eeed0b1b091419a16c95e8d0a3a369
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-03-28 19:07:07 +0600

    Date Sort Added In orders API

--- routes/purchase.js
+++ routes/purchase.js
@@ -27,7 +27,12 @@ router.post("/", async (req, res) => {
 
 router.get("/orders", async (req, res) => {
   try {
-    const result = await purchaseCollection.find().toArray();
+    const result = await purchaseCollection
+      .find()
+      .sort({
+        date: -1,
+      })
+      .toArray();
     res.send(result);
   } catch (error) {
     res.status(500).send({ message: error.message });
