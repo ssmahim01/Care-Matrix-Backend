f072bf272a40f113a206001a64f98fed52acdcd7
Arrafi2644 <raffian2644@gmail.com>
2025-03-30 22:18:06 +0600

    Add get appointments for patient

--- routes/appointments.js
+++ routes/appointments.js
@@ -3,15 +3,32 @@ import { collections, connectDB } from "../config/connectDB.js";
 import { ObjectId } from "mongodb";
 const router = express.Router();
 
+let appointmentsCollection;
+// Initialize Database Connection and Collections
+async function mongoDBCollection() {
+    try {
+        await connectDB();
+        appointmentsCollection = collections.appointments;
+    } catch (error) {
+        console.error("Error initializing database:", error);
+    }
+}
+
+// Ensure the database is initialized before handling routes
+mongoDBCollection();
+
+// Book appointments 
 router.post("/", async(req, res) => {
   const appointmentInfo = await req.body;
   const result = await collections.appointments.insertOne(appointmentInfo)
   res.send(result)
 
 })
+
+// Get appointments for receptionists
 router.get("/:email", async (req, res) => {
   const email = req.params.email;
-  console.log(email);
+  // console.log(email);
   const result = await collections.appointments.find().toArray()
   res.send(result);
 });
@@ -22,6 +39,16 @@ router.delete("/:id", async(req, res)=>{
  const result = await collections.appointments.deleteOne(query)
  res.send(result)
 })
+
+
+// Get appointments for patients
+router.get('/patients/:email', async(req, res)=>{
+  const email = req.params.email;
+  const query = {email: email}
+  const result = await appointmentsCollection.find(query).toArray()
+  res.send(result)
+})
+
 // Api endpoint -> /appointment
 
 export default router;
