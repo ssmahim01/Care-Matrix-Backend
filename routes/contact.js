8ad8b7a57a612d00afeb80abe08f3d73df93b2aa
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-16 22:22:25 +0600

    Delete Contact Message API route added

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { collections, connectDB } from "../config/connectDB.js";
+import { ObjectId } from "mongodb";
 const router = express.Router();
 
 let contactCollection;
@@ -29,14 +30,28 @@ router.post("/", async (req, res) => {
 
 router.get("/", async (req, res) => {
   try {
-    const result = await contactCollection.find().toArray();
+    const result = await contactCollection.find().sort({ _id: -1 }).toArray();
+
     const resultWithTimestamp = result.map((msg) => ({
       ...msg,
-      createdAt: msg._id.getTimestamp().toISOString(),
+      sentAt: msg._id.getTimestamp().toISOString(),
     }));
     res.send(resultWithTimestamp);
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await contactCollection.deleteOne({
+      _id: new ObjectId(id),
+    });
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+});
+
 export default router;
