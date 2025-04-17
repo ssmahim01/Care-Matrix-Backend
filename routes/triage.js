de7147c05fbd38a1f87258ed444668cb1d1813ba
MD Nafizul Iqram <nafizulikram3@gmail.com>
2025-04-17 14:17:31 +0600

    Delete route updated

--- routes/triage.js
+++ routes/triage.js
@@ -1,6 +1,7 @@
 
 import express from "express";
 import { connectDB } from "../config/connectDB.js";
+import { ObjectId } from "mongodb";
 
 
 const router = express.Router();
@@ -17,7 +18,7 @@ await initCollection();
 router.post("/add", async (req, res) => {
     try {
         const triage = req.body;
-        const result = await triageCollection.insertOne({status: "waiting", arrivalTime: new Date(),assignedDoctor: null, assignedRoom: null, ...triage});
+        const result = await triageCollection.insertOne({ status: "waiting", arrivalTime: new Date(), assignedDoctor: null, assignedRoom: null, ...triage });
         res.status(201).send({
             message: "Patient added successfully",
             triage,
@@ -30,9 +31,24 @@ router.post("/add", async (req, res) => {
     }
 })
 
-router.get("/all-triage", async (req, res)=> {
-   const results = await triageCollection.find().toArray();
-   res.send(results)  
+router.get("/all-triage", async (req, res) => {
+    const results = await triageCollection.find().toArray();
+    res.send(results)
+})
+
+router.delete("/delete-triage/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+
+    if (!id) return res.send({ message: "Id is required" })
+
+    const result = await triageCollection.deleteOne(query)
+
+    if (result.deletedCount === 1) {
+        res.send({ message: "Successfully deleted." });
+    } else {
+        res.send({ message: "No id matched the query." });
+    }
 })
 
 
