005738ab1bf12f6f1309ad0a01c933a303bc1c05
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-19 20:38:16 +0600

    assignUser Post API added

--- routes/assignUsers.js
+++ routes/assignUsers.js
@@ -44,9 +44,55 @@ admin.initializeApp({
 });
 
 router.post("/assign-user", async (req, res) => {
-  // Post user in firebase
-  // Get user info from firebase
-  // Post user in mongoDB
+  try {
+    const user = req.body;
+    const password = user?.password;
+
+    // check user email exists or not
+    const query = { email: user.email };
+    const isExist = await usersCollection.findOne(query);
+    if (isExist) {
+      return res.send(isExist);
+    }
+
+    let hashedPassword = null;
+    if (password) {
+      hashedPassword = await hashPassword(password);
+    }
+
+    // Post user in firebase
+    const result = await admin.auth().createUser({
+      email: user.email,
+      password: password,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+    });
+
+    // Get user info from firebase
+    const userInfo = {
+      role: user?.role,
+      email: user?.email,
+      name: user?.name,
+      password: hashedPassword,
+      photo: user?.photo,
+      phoneNumber: user?.phoneNumber,
+      uid: result?.uid,
+      createdAt: result?.metadata?.creationTime,
+      lastLoginAt: result?.metadata?.lastSignInTime,
+      providerId: "assigned",
+    };
+
+    // Post user in mongoDB
+    const postUser = await usersCollection.insertOne(userInfo);
+
+    res.send({
+      firebase: result,
+      mongoDB: postUser,
+      message: "User Created Successfully",
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 export default router;
