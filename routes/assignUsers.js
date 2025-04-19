3381da3971aee504aa90cb6566cc5262c804d12f
sheikh-saiyam <sheikhsaiyam29@gmail.com>
2025-04-19 20:25:41 +0600

    Firebase serviceAccount adde

--- routes/assignUsers.js
+++ routes/assignUsers.js
@@ -27,6 +27,22 @@ async function initCollection() {
 }
 initCollection();
 
+// serviceAccount
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+};
+
+// Intialize the firebase-admin project/account
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 router.post("/assign-user", async (req, res) => {
   // Post user in firebase
   // Get user info from firebase
