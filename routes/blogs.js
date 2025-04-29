65bd913a4e1aa7db844843664eb1ace2a7fcca8e
sayed725 <ssayed72533@gmail.com>
2025-04-29 15:46:21 +0600

    added specific blog get function

--- routes/blogs.js
+++ routes/blogs.js
@@ -17,12 +17,31 @@ await initCollection();
 // get all blog info
 router.get('/', async (req, res) => {
     try {
-        const result = await blogCollection.find().sort({ time: -1 }).toArray();
+        const result = await blogCollection.find().sort({ date: -1 }).toArray();
         res.send(result);
     } catch (error) {
         res.status(500).json({ error: 'failed to fetch blog info.' });
     }
 });
+
+// get blog data using id 
+
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const filter = {
+            _id: new ObjectId(id)
+        };
+        const result = await blogCollection.findOne(filter);
+        res.send(result);
+    } catch (error) {
+        res.status(500).json({ error: 'failed to fetch blog info.' });
+    }
+    }
+);
+
+
+
 
 // post blog info
 router.post('/', async (req, res) => {
@@ -31,6 +50,24 @@ router.post('/', async (req, res) => {
 
         const result = await blogCollection.insertOne(blogData);
         res.send(result);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// post delete info
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const filter = {
+            _id: new ObjectId(id)
+        };
+        // console.log(id);
+        const result = await blogCollection.deleteOne(filter);
+        res.send({
+            data: result,
+            message: "Blog Deleted Successfully!",
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
