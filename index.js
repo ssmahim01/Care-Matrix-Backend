382d637b3c6fe5dea1417eaa12bba1ea29a26218
ssmahim01 <mahimzayn666@gmail.com>
2025-08-27 22:43:14 +0600

    Installed packages then add one cors option to the index

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const corsOptions = {
     "http://localhost:5174",
     "http://localhost:4173",
     "https://care-matrix.web.app",
+    "https://care-matrix.vercel.app",
     "https://care-matrix-with-dev-sync-hub.vercel.app"
   ],
   credentials: true,
